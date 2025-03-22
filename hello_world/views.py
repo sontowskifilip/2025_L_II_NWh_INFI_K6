2814b1dda0523f21e184e8a4bd871a6be6964fa2
vdi-terminal <vdi-terminal@EDU-WSB.local>
2025-03-22 11:24:25 +0100

    poprawione komendy 3

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,10 +1,11 @@
 from hello_world import app
 from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
-from flask import request
+from flask import request # noqa
 
 moje_imie = "Filip"
 msg = "Hello World!"
+
 
 @app.route('/')
 def index():
@@ -13,6 +14,7 @@ def index():
         output = PLAIN
     return get_formatted(msg, moje_imie,
                          output.lower())
+
 
 @app.route('/outputs')
 def supported_output():
