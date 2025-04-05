61c7790094d36af486dd5f92a751dacfd071cf0c
sontowskifilip <filipson06@gmail.com>
2025-04-05 10:08:07 +0200

    srednia wazona

--- avg.php
+++ avg.php
@@ -1,1 +1,9 @@
-<?php function srednia($a, $b) { return ($a + $b) / 2; } ?>
+<?php
+function srednia($a, $b) {
+    return ($a + $b) / 2;
+}
+
+function srednia_wazona($a, $b, $waga1, $waga2) {
+    return ($a * $waga1 + $b * $waga2) / ($waga1 + $waga2);
+}
+?><?php function srednia($a, $b) { return ($a + $b) / 2; } ?>
